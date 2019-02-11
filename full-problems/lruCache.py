a4ecc388b2fb2ee8ac9aff4ec0ba5f9c3c1f7c81
Vikas Tiwari <vikas@optumsoft.com>
2019-02-11 22:09:14 +0530

    Add LRU cache

---
+++ full-problems/lruCache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+# Basic impl. of an LRU cache
+"""
+Least Recently Used (LRU): This cache algorithm keeps recently used items 
+near the top of cache. Whenever a new item is accessed, the LRU places 
+it at the top of the cache. When the cache limit has been reached, 
+items that have been accessed less recently will be removed starting 
+from the bottom of the cache.
+We need the order in which the item was accessed and the access time must be
+O(1). So an OrderedDict type seems perfect 
+""" 
+
+from collections import OrderedDict
+
+class LruCache:
+    def __init__(self, maxLen=100000):
+        self.cache = OrderedDict()
+        self.maxLen = maxLen
+    
+    def __setitem__(self, key, val):
+        if k in self.cache:
+            self.cache.pop(key)
+        
+        if len(self.cache) >= self.maxLen:
+            self.cache.popitem(last=False)
+        # Delete the value stored at last key, if length > maxLen
+        # last = False imposes FIFO
+        
+        self.cache[key] = val
+    
+    def __getitem__(self, key):
+        if key in self.cache:
+            val = self.cache.pop(key)
+            self.cache[key] = val
+            # Delete the value when found, and add it again, this changes
+            # the order, bringing the 'key' to the front thereby making
+            # it the recently used   
+            return val
+        else:
+            raise KeyError
+# Usecase examples
+x = LruCache()
+x[1] = 2 # set
+x[2] = 3 # set
+y = x[1] # get
