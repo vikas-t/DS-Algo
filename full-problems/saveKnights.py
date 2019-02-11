38c0e6d457211c89778399f726d2d8ac4e717ccd
Vikas Tiwari <vikas@optumsoft.com>
2019-02-11 21:49:12 +0530

    Fix comment

--- full-problems/saveKnights.py
+++ full-problems/saveKnights.py
@@ -5,7 +5,12 @@ import math
 
 def sol(n):
     """
-    Not very sure how it works. Wrapping up
+    knight, when on a black square, can only attack white squares. 
+    Likewise, a knight on a white square can only attack black squares. 
+    That means that if you only place your knights on the same colour 
+    squares on the board they will not attack one another.
+    So, lets say n = 5. Total squares = 25. 13 will be of one color, 12 of the
+    other. We take the bigger one
     """
     if n == 2: return 4
     return math.ceil(n*n/2)
