3ed77172bf6e8858b250c62bf639bd83ae8adf43
Vikas Tiwari <vikas@optumsoft.com>
2019-02-12 00:24:25 +0530

    Improve code, add recursive logic, fix indentation

--- full-problems/lis.py
+++ full-problems/lis.py
@@ -1,61 +1,56 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Longest increasing subsequence
 
 import sys 
 
 def longer_list(list1, list2):
-   if len(list1) > len(list2):
-      return list1
-   else:
-      return list2
+    if len(list1) > len(list2):
+        return list1
+    else:
+        return list2
 
 def lis_rec(list, last=-1*sys.maxint):
-   """
-   Recursive solution
-   """
-   if not(list):
-      return []
+    """
+    Recursive solution that prints the list
+    """
+    if not(list):
+        return []
    
-   incl = []
-   if list[0] > last:
-      incl = [list[0]] + lis_rec(list[1:], list[0])   
-   excl = lis_rec(list[1:], last)
+    incl = []
+    if list[0] > last:
+        incl = [list[0]] + lis_rec(list[1:], list[0])   
+    excl = lis_rec(list[1:], last)
 
-   return longer_list(incl, excl)
+    return longer_list(incl, excl)
 
+def listRec(arr, n, cur=0, prev=None):
+    if cur >= n:
+        return 0
+    
+    incl = 0
+    excl = 0
+    if prev == None or arr[cur] > arr[prev]:
+        incl = 1 + listRec(arr, n, cur+1, cur)
+    excl = listRec(arr, n, cur+1, prev)
+    
+    return max(incl, excl)
 
-def lis_dp(list):
-   n = len(list)
-   dp = [[list[0]]] * n
-
-   for i in xrange(1, n):
-      for j in xrange(0, i):
-         if list[i] > list[j] and len(dp[j]) + 1 > len(dp[i]):
-            dp[i] = dp[j] + [list[i]]
-      
-   m = []
-   for i in xrange(n):
-      m = longer_list(m, dp[i])
-      
-   return m
-
-
-def lis_dp2(arr):
-   """
-   Best solution
-   """
-   n = len(arr)
-   dp = [1]*n
-   for i in range(n):
-      for j in range(i):
-         if arr[j] <= arr[i]:
-         # Watch out if = is not needed
-            dp[i] = max(dp[i], dp[j]+1)
-            # Max of included or excluded
-   return max(dp)
-   # dp can also be empty, you can return 0 in that case
-   # Return the longest
+def lis_dp(arr):
+    """
+    Best solution
+    """
+    n = len(arr)
+    dp = [1]*n
+    for i in range(n):
+        for j in range(i):
+            if arr[j] <= arr[i]:
+            # Watch out if = is not needed
+                dp[i] = max(dp[i], dp[j]+1)
+                # Max of included or excluded
+    return max(dp)
+    # dp can also be empty, you can return 0 in that case
+    # Return the longest
 
 test_cases = [
    [1, 10, 2, 11, 12],
@@ -69,9 +64,8 @@ test_cases = [
    [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
 ]
 
-print lis_dp([1, 15, 19, 2])
-print ""
+# Driver code
 
 for case_list in test_cases:
-   print lis_dp(case_list)
-   print lis_dp2(case_list)
+    print lis_dp(case_list), listRec(case_list, len(case_list))
+    # Compare both solutions
